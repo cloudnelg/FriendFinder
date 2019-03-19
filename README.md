8b86446fac48b46e8240fe764ef5ab94c2afc0e9
c1oud <cloudnelg@gmail.com>
2019-03-18 22:03:23 -0700

    Friend Finder

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# FriendFinder
+# Friend Finder Application
+
+## Description
+
+This app compares your answers with other users and gives you the closest matching person.
+
+The app uses a Node.js and Express server on the back end and the Bootstrap framework on the front end.
+
+
+## Technologies Used:
+
+Javascript nodeJS expressJS npm packages:path body-parser CSS: Twitter Bootstrap
+
---
+++ app/data/friends.js
@@ -0,0 +1,116 @@
+// Array of friends to match //
+var friends = [{
+    "name": "Super Mario",
+    "photo": 'https://cdn02.nintendo-europe.com/media/images/08_content_images/news_5/2016_1/december_14/CI7_SuperMarioNews_01_CMM_big.jpg',
+    "scores": [
+        5,
+        1,
+        1,
+        3,
+        1,
+        3,
+        5,
+        5,
+        3,
+        1
+    ]
+},
+{
+    "name": "Luigi",
+    "photo": 'https://www.mariowiki.com/images/thumb/9/9d/Luigi_Artwork_-_Mario_Golf_World_Tour.png/170px-Luigi_Artwork_-_Mario_Golf_World_Tour.png',
+    "scores": [
+        1,
+        3,
+        3,
+        3,
+        1,
+        3,
+        5,
+        5,
+        5,
+        1
+    ]
+},
+{
+    "name": "Donkey Kong",
+    "photo": 'https://vignette.wikia.nocookie.net/vsbattles/images/8/83/1Iz5XD2.png/revision/latest?cb=20180613004739',
+    "scores": [
+        3,
+        5,
+        1,
+        1,
+        1,
+        5,
+        5,
+        3,
+        4,
+        2
+    ]
+},
+{
+    "name": "Ryu",
+    "photo": 'https://i0.wp.com/i.jeuxactus.com/datas/jeux/s/t/street-fighter-5/xl/street-fighter-5-56c5ad8ea9820.jpg',
+    "scores": [
+        2,
+        1,
+        1,
+        3,
+        3,
+        1,
+        3,
+        1,
+        1,
+        1
+    ]
+},
+{
+    "name": "Ganon",
+    "photo": 'https://i.pinimg.com/originals/5d/ca/ad/5dcaad4897a1271f1e1adc7f2b5b34cd.jpg',
+    "scores": [
+        1,
+        1,
+        5,
+        1,
+        5,
+        3,
+        5,
+        1,
+        1,
+        1
+    ]
+},
+{
+    "name": "Link",
+    "photo": 'https://vignette.wikia.nocookie.net/zelda/images/b/b1/Link_%28SSB_3DS_%26_Wii_U%29.png/revision/latest?cb=20130611202433',
+    "scores": [
+        1,
+        1,
+        4,
+        1,
+        5,
+        3,
+        5,
+        1,
+        1,
+        1
+    ]
+},
+{
+    "name": "Samus",
+    "photo": 'https://i.pinimg.com/474x/7f/f3/41/7ff34191087f84b3ce59cbcbf19e599f--metroid-samus-samus-aran.jpg',
+    "scores": [
+        2,
+        3,
+        3,
+        5,
+        5,
+        1,
+        5,
+        1,
+        1,
+        2
+    ]
+}
+]
+
+module.exports = friends;
